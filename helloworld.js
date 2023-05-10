f08f746ab3981a9acd1534a4bfbdc517e180e016
Josh19002 <josh123478@gmail.com>
2023-05-09 18:09:28 -0600

    Finished code for loop

--- helloworld.js
+++ helloworld.js
@@ -1,7 +1,12 @@
 const teamNames = [];
-teamNames.push("Joshua")
-teamNames.push("Brent")
+teamNames.push("Joshua");
+teamNames.push("Brent");
 teamNames.push("Austin");
 teamNames.push("JJ");
 
-console.log("Hello ");
+function helloTeam(teamNames){
+    for (i in teamNames){
+        console.log("Hello "+teamNames[i]);
+    };
+};
+helloTeam(teamNames);
